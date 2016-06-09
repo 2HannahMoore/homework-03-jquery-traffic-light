e3d6e4c9d3a6eb18e9bbde81119b0d3be72af001
2HannahMoore <hannah.moore@hannahmoore.jet.local>
2016-06-09 16:28:23 -0400

    Updated

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  console.log("hi hannah!");
+
+  $('#stopButton').click(function(){
+    $('#stopLight').css('background-color', 'red');
+  });
+
+  $('#slowButton').click(function(){
+    $('#slowLight').css('background-color', 'yellow');
+  });
+
+  $('#goButton').click(function(){
+   $('#goLight').css('background-color', 'green');
+  });
+
+});
